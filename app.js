128de18d86a61cd470d84c2b4d974650b77d2d98
Md Nurul Islam <nurulislamrimon>
2022-02-24 02:25:06 +0600

    modal added

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const modalContent = document.getElementById("modal-content");
+const closeBtn = document.getElementById("close-button");
+const popupContentContainer = document.getElementById("popup-content-container");
 const getCountries = () => {
     fetch("https://restcountries.com/v2/all")
         .then(stringData => stringData.json())
@@ -12,7 +15,7 @@ const displayCountries = countries => {
         <img class="shadow" width="200" height="100" src="${country.flag}">
         <h3>${country.name}</h3>
         <h5>Capital: <span class="color-red">${country.capital}</span></h5>
-        <button onclick="moreDetails('${country.name}')">Click to see details...</button>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="moreDetails('${country.name}')">Click to see details...</button>
         `;
         listContainer.appendChild(card);
     });
@@ -24,20 +27,22 @@ const moreDetails = (name) => {
 }
 const showMoreDetails = showDataCountries => {
     showDataCountries.forEach(showDataCountry => {
-        console.log(showDataCountry);
-        alert(`
-        Name: ${showDataCountry.name}
-        Capital: ${showDataCountry.capital}
-        Total area: ${showDataCountry.area}
-        Population: ${showDataCountry.population}
-        Currencies: ${showDataCountry.currencies}
-        Languages: ${showDataCountry.languages}
-        Borders with: ${showDataCountry.borders}
-        Calling Code: ${showDataCountry.callingCodes}
-        Timezones: ${showDataCountry.timezones}
-        Region: ${showDataCountry.region}
-        Are Independent: ${showDataCountry.independent}
-        `)
+        modalContent.innerHTML = `
+        <h3><span class="popup-left-content">Name :</span> <span>${showDataCountry.name}</span></h3>
+        <h3><span class="popup-left-content">Capital :</span> <span>${showDataCountry.capital}</span></h3>
+        <h3><span class="popup-left-content">Total area :</span> <span>${showDataCountry.area}</span></h3>
+        <h3><span class="popup-left-content">Population :</span> <span>${showDataCountry.population}</span></h3>
+        <h3><span class="popup-left-content">Currencies :</span> <span>${showDataCountry.currencies}</span></h3>
+        <h3><span class="popup-left-content">Languages :</span> <span>${showDataCountry.languages}</span></h3>
+        <h3><span class="popup-left-content">Borders with :</span> <span>${showDataCountry.borders}</span></h3>
+        <h3><span class="popup-left-content">Calling Code :</span> <span>${showDataCountry.callingCodes}</span></h3>
+        <h3><span class="popup-left-content">Timezones :</span> <span>${showDataCountry.timezones}</span></h3>
+        <h3><span class="popup-left-content">Region :</span> <span>${showDataCountry.region}</span></h3>
+        <h3><span class="popup-left-content">Are Independent :</span> <span>${showDataCountry.independent}</span></h3>     
+        `
     })
 }
 getCountries();
+
+// ===========closeBtn=========
+closeBtn.addEventListener("click", () => modalContent.style.display = "none")
